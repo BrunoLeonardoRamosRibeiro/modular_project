bb17e497c25818ec5e493158f03300197d9b3b9a
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-05-26 15:42:26 -0300

    Build a Dio Factory for Network Layer

---
+++ core/data/lib/factory/dio_factory.dart
@@ -0,0 +1,41 @@
+import 'package:data/constants/constants.dart';
+import 'package:dio/dio.dart';
+import 'package:flutter/foundation.dart';
+import 'package:pretty_dio_logger/pretty_dio_logger.dart';
+
+class DioFactory {
+  DioFactory();
+
+  Future<Dio> getDio() async {
+    Dio dio = Dio();
+
+    Map<String, String> headers = {
+      contentType: applicationJson,
+      accept: applicationJson,
+      authorization: "your_access_token", // TODO: get access token here
+      defaultLanguage: "app_language", // TODO: get app lang here
+      clientId: "app_client", // TODO: get app client  id here
+    };
+
+    dio.options = BaseOptions(
+      baseUrl: "your_base_url",
+      headers: headers,
+      receiveTimeout: const Duration(seconds: 60),
+      sendTimeout: const Duration(seconds: 60),
+    );
+
+    // add logger in debug mode only NOT release
+    if (!kReleaseMode) {
+      dio.interceptors.add(
+        PrettyDioLogger(
+          requestHeader: true,
+          requestBody: true,
+          responseHeader: true,
+          responseBody: true,
+        ),
+      );
+    }
+
+    return dio;
+  }
+}
