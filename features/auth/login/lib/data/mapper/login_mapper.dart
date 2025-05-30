bdd1041280656dd52a57690b16908a1e147fd6fb
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-05-30 13:58:21 -0300

    Update Login Mapper with extensions

--- features/auth/login/lib/data/mapper/login_mapper.dart
+++ features/auth/login/lib/data/mapper/login_mapper.dart
@@ -1,12 +1,13 @@
+import 'package:extensions/extensions.dart';
 import 'package:login/data/response/login_response.dart';
 import 'package:login/domain/model/login_model.dart';
 
 extension LoginResponseMapper on LoginResponse? {
   LoginModel toDomain() {
     return LoginModel(
-      name: this?.name ?? "",
-      phone: this?.phone ?? "",
-      age: this?.age ?? 0,
+      name: this!.name.orEmpty(),
+      phone: this!.phone.orEmpty(),
+      age: this!.age.orZero(),
     );
   }
 }
