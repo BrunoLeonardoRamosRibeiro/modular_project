2174ea7e7ff0a16292a19c884cd05f1bb65de743
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-06-03 14:46:12 -0300

    Enhancing Safe API Call witn network Connectivity Check

--- core/data/lib/network/api_safe_call.dart
+++ core/data/lib/network/api_safe_call.dart
@@ -1,14 +1,28 @@
 import 'package:dartz/dartz.dart';
+import 'package:data/network_info/network_info.dart';
 import 'package:domain/model/failure.dart';
 import 'package:domain/model/localised_message.dart';
 
-Future<Either<Failure, T>> safeApiCall<T>(Future<T> Function() apiCall) async {
-  try {
-    final response = await apiCall();
-    return Right(response);
-  } catch (error) {
+Future<Either<Failure, T>> safeApiCall<T>(
+  NetworkInfo networkInfo,
+  Future<T> Function() apiCall,
+) async {
+  if (await networkInfo.isConnected) {
+    try {
+      final response = await apiCall();
+      return Right(response);
+    } catch (error) {
+      return Left(
+        Failure(0, LocalisedMessage("", "")),
+      ); // TODO: ADD ERROR HANDLER HERE
+    }
+  } else {
+    //  no internet connection
     return Left(
-      Failure(0, LocalisedMessage("", "")),
-    ); // TODO: ADD ERROR HANDLER HERE
+      Failure(
+        -500,
+        LocalisedMessage("network connectivity issue, please check", ""),
+      ),
+    );
   }
 }
