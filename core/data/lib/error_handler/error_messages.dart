ea91bc254cf85337a896f2dd6f028a0e4ac468d9
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-06-05 14:23:40 -0300

    Implements error messages

---
+++ core/data/lib/error_handler/error_messages.dart
@@ -0,0 +1,15 @@
+class AppStrings {
+  static const String success = "success";
+  static const String noContent = "no_content";
+  static const String badRequest = "bad_request";
+  static const String forbidden = "forbidden";
+  static const String unauthorized = "unauthorized";
+  static const String internalServerError = "internal_server_error";
+  static const String connectTimeout = "connect_time_out";
+  static const String cancelled = "cancelled";
+  static const String receiveTimeout = "receive_time_out";
+  static const String sendTimeout = "send_time_out";
+  static const String cacheError = "cache_error";
+  static const String noInternetConnection = "no_internet_connection";
+  static const String defaultError = "default_error";
+}
