ee39e25b1ea0a1bcf72719700d20efc1f9818e3e
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-06-10 20:18:59 -0300

    Update injection

--- core/data/lib/di/injection.dart
+++ core/data/lib/di/injection.dart
@@ -1,6 +1,6 @@
 import 'package:get_it/get_it.dart';
 import 'package:injectable/injectable.dart';
-import './injection.config.dart';
+import 'package:data/di/injection.config.dart';
 
 final getIt = GetIt.instance;
 
