944a76aa6b9909b985007dff8cedcb0d4d4aa7c9
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-06-16 19:25:39 -0300

    Test login

--- features/auth/login/lib/login.dart
+++ features/auth/login/lib/login.dart
@@ -1,9 +1,17 @@
+import 'package:login/data/request/login_request.dart';
+import 'package:modular_project/di/injection.dart';
 import 'package:datastore/provider/session/session_provider_impl.dart';
 import 'package:dio/dio.dart';
+import 'package:login/domain/usecase/login_usecase.dart';
 
 /// A Calculator.
 class Calculator {
   /// Returns [value] plus 1.
   int addOne(int value) => value + 1;
   Dio dio = Dio();
+}
+
+void doLogin() async {
+  final loginUsecase = getIt<LoginUseCase>();
+  final loginRequest = LoginRequest("mina@gmail.com", "123456");
 }
