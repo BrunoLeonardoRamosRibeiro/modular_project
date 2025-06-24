374e7a808e51be8eb8b1c7e507f70df65a880150
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-06-24 14:42:45 -0300

    Create State Rendeder

---
+++ core/presentation/lib/state_renderer/state_renderer.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:presentation/state_renderer/state_renderer_type.dart';
+
+class StateRenderer extends StatelessWidget {
+  final StateRendererType stateRendererType;
+
+  const StateRenderer({super.key, required this.stateRendererType});
+
+  @override
+  Widget build(BuildContext context) {
+    switch (stateRendererType) {
+      case StateRendererType.popupLoadingState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.popupErrorState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.fullScreenLoadingState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.fullScreenErrorState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.emptyState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.contentState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.nome:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+    }
+    ;
+  }
+}
