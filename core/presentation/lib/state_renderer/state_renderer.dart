536174d99f1b6af63521ce95ad5606357a3ea0af
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-06-25 20:26:21 -0300

    Full Screen Error state page

--- core/presentation/lib/state_renderer/state_renderer.dart
+++ core/presentation/lib/state_renderer/state_renderer.dart
@@ -5,43 +5,42 @@ class StateRenderer extends StatelessWidget {
   final StateRendererType stateRendererType;
   final String message;
   final String title;
+  final VoidCallback? retryActionFunction;
 
   StateRenderer({
     super.key,
     required this.stateRendererType,
     this.message = "Loading...",
     this.title = "Error",
+    this.retryActionFunction,
   });
 
   var _isDialogDismissed = false;
   var _isDialogShowing = false;
 
   _isThereCurrentDialogShowing(BuildContext context) =>
-      ModalRoute
-          .of(context)
-          ?.isCurrent != true;
+      ModalRoute.of(context)?.isCurrent != true;
 
   @override
   Widget build(BuildContext context) {
     switch (stateRendererType) {
-    case StateRendererType.popupLoadingState:
-    return _showPopupLoadingDialog(context, _buildLoadingWidget());
-    case StateRendererType.popupErrorState:
-    return _showPopupErrorDialog(context, _buildErrorWidget());
-    case StateRendererType.fullScreenLoadingState:
-    return _showFullScreenLoading(_buildLoadingWidget());
-    case StateRendererType.fullScreenErrorState:
-    // TODO: Handle this case.
-    throw UnimplementedError();
-    case StateRendererType.emptyState:
-    // TODO: Handle this case.
-    throw UnimplementedError();
-    case StateRendererType.contentState:
-    // TODO: Handle this case.
-    throw UnimplementedError();
-    case StateRendererType.nome:
-    // TODO: Handle this case.
-    throw UnimplementedError();
+      case StateRendererType.popupLoadingState:
+        return _showPopupLoadingDialog(context, _buildLoadingWidget());
+      case StateRendererType.popupErrorState:
+        return _showPopupErrorDialog(context, _buildErrorWidget());
+      case StateRendererType.fullScreenLoadingState:
+        return _showFullScreenContent(_buildLoadingWidget());
+      case StateRendererType.fullScreenErrorState:
+        return _showFullScreenContent(_buildErrorWidget(showRetryButton: true));
+      case StateRendererType.emptyState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.contentState:
+        // TODO: Handle this case.
+        throw UnimplementedError();
+      case StateRendererType.nome:
+        // TODO: Handle this case.
+        throw UnimplementedError();
     }
   }
 
@@ -56,7 +55,7 @@ class StateRenderer extends StatelessWidget {
     );
   }
 
-  Widget _buildErrorWidget() {
+  Widget _buildErrorWidget({bool showRetryButton = false}) {
     return Column(
       mainAxisSize: MainAxisSize.min,
       children: [
@@ -67,11 +66,14 @@ class StateRenderer extends StatelessWidget {
           style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
         ),
         Text(message),
+        SizedBox(height: 10),
+        if (showRetryButton)
+          ElevatedButton(onPressed: retryActionFunction, child: Text("Retry")),
       ],
     );
   }
 
-  Widget _showFullScreenLoading(Widget content) {
+  Widget _showFullScreenContent(Widget content) {
     return Container(
       color: Colors.white,
       child: Center(child: content),
@@ -82,11 +84,10 @@ class StateRenderer extends StatelessWidget {
     if (!_isThereCurrentDialogShowing(context)) {
       _isDialogShowing = true;
       WidgetsBinding.instance.addPersistentFrameCallback(
-            (_) =>
-            showDialog(
-              context: context,
-              builder: (context) => AlertDialog(content: content),
-            ),
+        (_) => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(content: content),
+        ),
       );
     }
     return Container();
@@ -96,23 +97,21 @@ class StateRenderer extends StatelessWidget {
     if (!_isThereCurrentDialogShowing(context)) {
       _isDialogShowing = true;
       WidgetsBinding.instance.addPersistentFrameCallback(
-            (_) =>
-            showDialog(
-              context: context,
-              builder: (context) =>
-                  AlertDialog(
-                    content: content,
-                    actions: [
-                      TextButton(
-                        onPressed: () {
-                          _isDialogDismissed = true;
-                          Navigator.of(context, rootNavigator: true).pop(true);
-                        },
-                        child: Text("Close"),
-                      ),
-                    ],
-                  ),
-            ),
+        (_) => showDialog(
+          context: context,
+          builder: (context) => AlertDialog(
+            content: content,
+            actions: [
+              TextButton(
+                onPressed: () {
+                  _isDialogDismissed = true;
+                  Navigator.of(context, rootNavigator: true).pop(true);
+                },
+                child: Text("Close"),
+              ),
+            ],
+          ),
+        ),
       );
     }
     return Container();
