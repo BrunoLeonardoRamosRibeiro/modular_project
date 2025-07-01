8d0506bb1db451a5d1a18db9b392a0d1aedf92be
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-07-01 09:37:26 -0300

    Update Login screen

--- features/auth/login/lib/presentation/login_screen.dart
+++ features/auth/login/lib/presentation/login_screen.dart
@@ -5,6 +5,8 @@ import 'package:login/presentation/login_bloc.dart';
 import 'package:login/presentation/login_event.dart';
 import 'package:login/presentation/login_state.dart';
 import 'package:modular_project/di/injection.dart';
+import 'package:presentation/state_renderer/state_renderer.dart';
+import 'package:presentation/state_renderer/state_renderer_type.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({super.key});
@@ -27,53 +29,64 @@ class _LoginScreenState extends State<LoginScreen> {
         create: (context) => LoginBloc(loginUsecase),
         child: BlocBuilder<LoginBloc, LoginState>(
           builder: (context, state) {
-            return Padding(
-              padding: const EdgeInsetsGeometry.all(16),
-              child: Column(
-                children: [
-                  TextField(
-                    controller: usernameController,
-                    onChanged: (value) {
-                      context.read<LoginBloc>().add(UsernameChanged(value));
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Username",
-                      errorText: state is LoginInvalid
-                          ? state.usernameError
-                          : null,
-                    ),
-                  ),
-                  const SizedBox(height: 8),
-                  TextField(
-                    controller: passwordController,
-                    onChanged: (value) {
-                      context.read<LoginBloc>().add(PasswordChanged(value));
-                    },
-                    decoration: InputDecoration(
-                      labelText: "Password",
-                      errorText: state is LoginInvalid
-                          ? state.passwordError
-                          : null,
-                    ),
-                  ),
-                  const SizedBox(height: 20),
-                  ElevatedButton(
-                    onPressed: () {
-                      context.read<LoginBloc>().add(
-                        LoginButtonPressed(
-                          usernameController.text,
-                          passwordController.text,
-                        ),
-                      );
-                    },
-                    child: Text("Login"),
-                  ),
-                ],
-              ),
+            return Stack(
+              children: [
+                _buildMainScreenContent(context, state),
+                _buildStateRenderer(context, state),
+              ],
             );
           },
         ),
       ),
+    );
+  }
+
+  Widget _buildMainScreenContent(BuildContext context, LoginState state) {
+    return Padding(
+      padding: const EdgeInsetsGeometry.all(16),
+      child: Column(
+        children: [
+          TextField(
+            controller: usernameController,
+            onChanged: (value) {
+              context.read<LoginBloc>().add(UsernameChanged(value));
+            },
+            decoration: InputDecoration(
+              labelText: "Username",
+              errorText: state is LoginInvalid ? state.usernameError : null,
+            ),
+          ),
+          const SizedBox(height: 8),
+          TextField(
+            controller: passwordController,
+            onChanged: (value) {
+              context.read<LoginBloc>().add(PasswordChanged(value));
+            },
+            decoration: InputDecoration(
+              labelText: "Password",
+              errorText: state is LoginInvalid ? state.passwordError : null,
+            ),
+          ),
+          const SizedBox(height: 20),
+        ],
+      ),
+    );
+  }
+
+  Widget _buildStateRenderer(BuildContext context, LoginState state) {
+    if (state.stateRendererType == StateRendererType.contentState) {
+      // display any overlay
+      return SizedBox.shrink();
+    }
+
+    return StateRenderer(
+      stateRendererType: state.stateRendererType,
+      message: (state is LoginError) ? state.errorMessage ?? "" : "",
+      retryActionFunction: () {
+        context.read<LoginBloc>().add(
+          LoginButtonPressed(usernameController.text, passwordController.text),
+        );
+      },
     );
   }
 }
