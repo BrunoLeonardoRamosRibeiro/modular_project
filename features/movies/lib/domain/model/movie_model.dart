7e02e6437aaa09352cb3696c557df1c90dcf930b
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-07-17 10:16:28 -0300

    Create movie model

---
+++ features/movies/lib/domain/model/movie_model.dart
@@ -0,0 +1,21 @@
+class Movie {
+  String id;
+  String originalTitle;
+  String overview;
+  String posterPath;
+  String releaseDate;
+  String title;
+  String voteAverage;
+  // CartNames? cartNames;
+
+  Movie({
+    required this.id,
+    required this.originalTitle,
+    required this.overview,
+    required this.posterPath,
+    required this.releaseDate,
+    required this.title,
+    required this.voteAverage,
+    // required this.cartNames,
+  });
+}
