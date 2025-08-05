ca93a43760fcc87e689d614a2aa38bd741a90d9e
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-08-05 15:57:24 -0300

    Implements movies cache datasource impl

---
+++ features/movies/lib/data/source/cache/movies_cache_data_source_impl.dart
@@ -0,0 +1,41 @@
+import 'package:hive/hive.dart';
+import 'package:movies/data/source/cache/movies_cache_data_source.dart';
+import 'package:movies/domain/model/cached_movie.dart';
+
+class MoviesCacheDataSourceImpl extends MoviesCacheDataSource {
+  static const _moviesCacheKey = 'moviesCacheKey';
+  static const _cacheExpiryKey = 'cacheExpiryKey';
+  static const _moviesCacheExpiryInSeconds = 60 * 10;
+
+  final Box _box;
+
+  MoviesCacheDataSourceImpl(this._box);
+
+  @override
+  Future<void> cacheMovies(List<CachedMovie> movies) async {
+    await _box.put(_moviesCacheKey, movies);
+    await _box.put(
+      _cacheExpiryKey,
+      DateTime.now()
+          .add(const Duration(seconds: _moviesCacheExpiryInSeconds))
+          .toIso8601String(),
+    );
+  }
+
+  @override
+  Future<List<CachedMovie>> getMovies() async {
+    return (_box.get(_moviesCacheKey, defaultValue: <CachedMovie>[]) as List)
+        .cast<CachedMovie>();
+  }
+
+  @override
+  Future<bool> hasValidCache() async {
+    final cacheExpiryTimeString = _box.get(_cacheExpiryKey);
+
+    if (cacheExpiryTimeString == null) return false;
+
+    final cacheExpiryTime = _box.get(cacheExpiryTimeString);
+
+    return DateTime.now().isBefore(cacheExpiryTime);
+  }
+}
