bcb4c0d09d95b48f990264b418c1058f5d3a5ca5
Bruno Leonardo Ramos Ribeiro <louzeiroribeiro@gmail.com>
2025-08-07 15:56:11 -0300

    Update movies repository impl with movies local datasource

--- features/movies/lib/data/repository/movies_repository_impl.dart
+++ features/movies/lib/data/repository/movies_repository_impl.dart
@@ -2,6 +2,7 @@ import 'package:dartz/dartz.dart';
 import 'package:domain/model/failure.dart';
 import 'package:movies/data/source/cache/movies_local_data_source.dart';
 import 'package:movies/data/source/remote/movies_remote_data_source.dart';
+import 'package:movies/domain/mapper/cache_mapper.dart';
 
 import 'package:movies/domain/mapper/movies_mapper.dart';
 import 'package:movies/domain/model/movie.dart';
@@ -15,10 +16,28 @@ class MoviesRepositoryImpl implements MoviesRepository {
 
   @override
   Future<Either<Failure, List<Movie>>> getMovies() async {
-    final result = await moviesRemoteDataSource.getMovies();
-    return result.fold(
-      (failure) => Left(failure),
-      (response) => Right(response.toDomain()),
-    );
+    if (await moviesLocalDataSource.hasValidCache()) {
+      final cachedMovies = await moviesLocalDataSource.getMovies();
+      return Right(
+        cachedMovies.map((cachedMovie) => cachedMovie.toMovie()).toList(),
+      );
+    } else {
+      final result = await moviesRemoteDataSource.getMovies();
+      return await result.fold(
+        (failure) {
+          return Left(failure);
+        },
+        (response) async {
+          await moviesLocalDataSource.cacheMovies(
+            response.movies!
+                .map(
+                  (movieResponse) => movieResponse.toDomain().toCachedMovie(),
+                )
+                .toList(),
+          );
+          return Right(response.toDomain());
+        },
+      );
+    }
   }
 }
